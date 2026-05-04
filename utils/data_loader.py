28d65c3757ae2fe0a9735828f7fb25561cae3425
milos <milos@MacBook-Air-de-Academie2.local>
2026-05-04 14:54:03 +0200

    Debut de main

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -9,12 +9,12 @@ class MNISTLoader:
 
     def load_data(self):
         """Charge le CSV ou les binaires MNIST."""
+        
         data = pd.read_csv(self.filepath)
+        n_samples = data.shape[0]
         labels = data.iloc[:, 0].values
         images = data.iloc[:, 1:].values
-
-        images = images.reshape(60000, 784)
-        labels = labels.reshape(60000,10)
+        images = images.reshape(n_samples, -1)[:, :784]
         """jevaispeteruncablesurgithubmaisc'estpasgravec'estlaviekonmenetuconnaisninhoouaisswizeuppourlanouvellemixtapec'esstvraimentchiantje veux juste ajouterdeuxlignesetgithubmetunnelsurmodifierlabranchmais commeon dit la vie est faite de branches auxquelles il faut s'agripper afin de rester etanche et de ne pas finir sous l'eau sinon tu n'es plus un bateau et un bateau c'est cool un peu comme une mouette ou un goeland quoi que ils sont plutot chiants les goelands mais ils chient en volant"""
 
         return images, labels
