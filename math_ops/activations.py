8b03059b1032c971d8de06b116faab82e523f24c
pauldebise <paul.debise@gmail.com>
2026-05-07 19:01:28 +0200

    Improving activations.py object encapsulation.

--- math_ops/activations.py
+++ math_ops/activations.py
@@ -4,48 +4,51 @@ from core.layer import Layer
 
 class ActivationFunction(ABC):
     """
-    Classe abstraite pour le pattern Strategy des fonctions d'activation.
+    Abstract class for activation functions.
     """
     @abstractmethod
     def compute(self, x):
         pass
 
     @abstractmethod
-    def derivative(self, x):
+    def backward(self, x, output_gradient):
         pass
 
 
 class ReLU(ActivationFunction):
-    """Fonction d'activation Rectified Linear Unit."""
+    """Activation function Rectified Linear Unit."""
     def compute(self, x):
         return np.maximum(0, x)
 
-    def derivative(self, x):
-        return np.where(x > 0, 1.0, 0.0)
+    def backward(self, x, output_gradient):
+        return output_gradient * np.where(x > 0, 1.0, 0.0)
 
 class LeakyReLU(ActivationFunction):
-    """Fonction d'activation Rectified Linear Unit."""
+    """Activation function Leaky Rectified Linear Unit."""
     def compute(self, x):
         return np.maximum(0.1*x, x)
 
-    def derivative(self, x):
-        return np.where(x > 0, 1.0, 0.1)
+    def backward(self, x, output_gradient):
+        return output_gradient * np.where(x > 0, 1.0, 0.1)
 
 class Sigmoid(ActivationFunction):
-    """Fonction d'activation Sigmoïde."""
+    """Activation function Sigmoid."""
     def compute(self, x):
         return 1 / (1 + np.exp(-x))
 
-    def derivative(self, x):
-        
+    def backward(self, x, output_gradient):
         sigmoid = self.compute(x)
-        return sigmoid * (1 - sigmoid)
+        return output_gradient * (sigmoid * (1 - sigmoid))
     
 class Softmax(ActivationFunction):
-    """Fonction d'activation Softmax."""
+    """Activation function Softmax."""
     def compute(self, x):
         x_norm = x - np.max(x, axis=1, keepdims=True)
         return np.exp(x_norm) / np.sum(np.exp(x_norm), axis=1, keepdims=True)
+
+    def backward(self, x, output_gradient):
+        s = self.compute(x)
+        return s * (output_gradient - np.sum(output_gradient * s, axis=1, keepdims=True))
 
     def derivative(self, x):
         softmax = self.compute(x)
@@ -56,17 +59,20 @@ class Softmax(ActivationFunction):
 
 
 class ActivationLayer(Layer):
+    """A layer without parameters that just applies a mathematical activation function."""
     def __init__(self, activation_strategy):
         super().__init__()
-        self.activation = activation_strategy
+        self.__activation = activation_strategy
         self.input = None
+
+    @property
+    def activation(self):
+        return self.__activation
+    #No setter because activation should not be changed after layer creation.
 
     def forward(self, input_data):
         self.input = input_data
         return self.activation.compute(self.input)
 
     def backward(self, output_gradient, learning_rate):
-        if isinstance(self.activation, Softmax):
-            s = self.activation.compute(self.input)
-            return s * (output_gradient - np.sum(output_gradient * s, axis=1, keepdims=True))
-        return output_gradient * self.activation.derivative(self.input)
+        return self.activation.backward(self.input, output_gradient)
